67827ebf01b8ed95932dc2eaac33eb69f206e2aa
Albert Espinos <albertes01@icloud.com>
2019-09-27 11:25:07 -0500

    Different way to create type List and add icons like a  menú

--- components/lib/pages/home_page.dart
+++ components/lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +11,51 @@ class HomePage extends StatelessWidget {
         title: Text('Componenete Temporal'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text("ListTile Title"),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          ListTile(
-            title: Text('ListTile Title'),
-          )
-        ],
+        children: _crearItemsCorta() 
       ),
-    )
+    );
   }
+
+  
+
+ /* List<Widget> _crearItems(){
+
+    List<Widget> lista = new List<Widget>();
+
+    for (var opt in opciones) {
+    
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)
+          ..add(Divider());
+    }
+
+    return lista;
+  }
+  */
+
+    List<Widget> _crearItemsCorta() {
+      
+      return opciones.map( (item){
+
+        return Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(item + "!"),
+              subtitle: Text('Something of something in Dart'),
+              leading: Icon(Icons.account_balance_wallet),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: (){},
+            ),
+            Divider()
+
+          ],
+        );
+
+      }).toList();
+
+    }
+
 }
